79398e86bc694e4f2277275678371f46c7efa818
Tomaz Saraiva <tomaz.saraiva@gmail.com>
2024-04-23 20:53:54 +0100

    add namespace

---
+++ Assets/DemoComponent.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DummyNamespace
+{
+    /// <summary>
+    /// DemoComponent Summary
+    /// </summary>
+    public class DemoComponent : MonoBehaviour
+    {
+        /// <summary>
+        /// Dummy Method Summary
+        /// </summary>
+        private void DummyMethod()
+        {
+
+        }
+    }
+}
